7d5a5061491623b63f045f9012c703f25677c50e
dominhdang148 <myclone235@gmail.com>
2024-01-07 23:55:03 +0700

    Add Brand Mapping to MS SQL

---
+++ src/WeebShop/WeebShop.Data/Mappings/BrandMap.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeebShop.Core.Entities;
+
+namespace WeebShop.Data.Mappings
+{
+   public class BrandMap: IEntityTypeConfiguration<Brand>
+    {
+       public void Configure(EntityTypeBuilder<Brand> builder)
+        {
+            builder.ToTable("Brands");
+            builder.HasKey(a => a.Id);
+            builder.Property(a => a.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+            builder.Property(a => a.UrlSlug)
+                .IsRequired()
+                .HasMaxLength(100);
+            builder.Property(a => a.LogoUrl)
+                .HasMaxLength(500);
+            builder.Property(a => a.Description)
+                .HasMaxLength(1000);
+        }
+    }
+}
