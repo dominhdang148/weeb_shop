bdd5a74d3b8da58362b45b9255851ec819863ff6
dominhdang148 <myclone235@gmail.com>
2024-01-08 02:35:33 +0700

    Add Tag Mapping into MS SQL

---
+++ src/WeebShop/WeebShop.Data/Mappings/TagMap.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeebShop.Core.Entities;
+
+namespace WeebShop.Data.Mappings
+{
+    public class TagMap:IEntityTypeConfiguration<Tag>
+    {
+        public void Configure(EntityTypeBuilder<Tag> builder)
+        {
+            builder.ToTable("Tags");
+            builder.HasKey(t => t.Id);
+            builder.Property(t => t.Name)
+                .HasMaxLength(50)
+                .IsRequired();
+            builder.Property(t => t.Description)
+                .HasMaxLength(100);
+            builder.Property(t => t.UrlSlug)
+                .HasMaxLength(50)
+                .IsRequired();
+        }
+    }
+}
