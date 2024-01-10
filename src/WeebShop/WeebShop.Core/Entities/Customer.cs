c1c326c5667e640f30cb15526cf3679a38f919c8
dominhdang148 <myclone235@gmail.com>
2024-01-11 02:51:07 +0700

    Create Entities for Customers

--- src/WeebShop/WeebShop.Core/Entities/Customer.cs
+++ src/WeebShop/WeebShop.Core/Entities/Customer.cs
@@ -10,13 +10,25 @@ namespace WeebShop.Core.Entities
 {
     public class Customer:IEntity, IUser
     {
+        #region Primary Key
         public int Id { get; set; }
+        #endregion
+
+        #region Properites
         public string Email { get;set; }
         public string Password { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set ; }
         public DateTime DateOfBirth { get; set; }
         public DateTime CreatedAt { get; set; }
-        public DateTime ModifiedAt { get; set; }    
+        public DateTime ModifiedAt { get; set; }
+        #endregion
+
+        #region Relationships
+        // One customer account can have many addresses
+        public IList<Address> Addresses { get; set; }
+        // One customer account can have many payment methods
+        public IList<PaymentMethod> PaymentMethods { get; set; }
+        #endregion
     }
 }
---
+++ src/WeebShop/WeebShop.Core/Entities/PaymentMethod.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeebShop.Core.Contracts;
+
+namespace WeebShop.Core.Entities
+{
+    public class PaymentMethod : IEntity
+    {
+        #region Primary Key
+
+        public int Id { get; set; }
+
+        #endregion
+
+        #region Properties
+
+        public string Provider { get; set; }
+        public string AccountNo { get; set; }
+        public DateTime Expiry { get; set; }
+
+        #endregion
+
+        #region Foreign Key
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+        #endregion
+
+
+    }
+}
