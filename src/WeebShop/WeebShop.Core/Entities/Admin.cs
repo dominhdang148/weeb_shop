1a8f81c1a8dc5885ef02a31d5e95e7943b96f0ce
dominhdang148 <myclone235@gmail.com>
2024-01-12 02:49:19 +0700

    Add Admin, Thinking about roles

---
+++ src/WeebShop/WeebShop.Core/Entities/Admin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeebShop.Core.Contracts;
+
+namespace WeebShop.Core.Entities
+{
+    public class Admin : IEntity, IUser
+    {
+        #region Primary Key
+        public int Id { get; set; }
+        #endregion
+
+        #region Properties
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+        #endregion
+
+
+    }
+}
